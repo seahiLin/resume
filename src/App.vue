893e53ab03025fac601222511b4ecb1fce202093
seahiLin <seahi666@outlook.com>
2018-10-03 22:42:33 +0800

    introduction page

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <introduction></introduction>
+  </div>
+</template>
+
+<script>
+
+import introduction from './pages/introduction/introduction'
+
+export default {
+  name: 'App',
+  components: {
+    introduction
+  }
+}
+</script>
+
+<style lang="less">
+@import './assets/styles/common.less';
+@import './assets/styles/reset.less';
+</style>
+
---
+++ src/pages/introduction/introduction.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page introduction">
+    <div class="avatar">
+      <img src="../../assets/images/IMG_1306.jpg" alt="">
+    </div>
+    <h1 class="name">林喜海</h1>
+    <p class="description">初步掌握面向搜索引擎编程</p>
+    <div class="information">
+      <span><img src="../../assets/icons/生日.svg" alt="生日" class="icon"> 1997.07</span>
+      <span><img src="../../assets/icons/求职意向.svg" alt="求职意向" class="icon">前端实习</span>
+      <span><img src="../../assets/icons/学校.svg" alt="学校" class="icon">河南科技大学-通信工程</span>
+      <span><img src="../../assets/icons/电话.svg" alt="电话" class="icon">156 3794 2652</span>
+      <span><img src="../../assets/icons/邮箱.svg" alt="邮箱" class="icon">22968 09118@qq.com</span>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+@import 'introduction.less';
+</style>
